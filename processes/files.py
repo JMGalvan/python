bf1dbccdeb6bc39bd58d6dcf50b597beeef2a780
JMGalvan <galvan_diaz@hotmail.com>
2021-03-28 14:52:25 +0200

    Add delete files saved and save file

--- processes/files.py
+++ processes/files.py
@@ -7,12 +7,27 @@
 # Processes
 from processes import translate
 from processes import common
+from processes import configuration
+from processes import types_rockets
+from processes import station_requests
+from processes import launchs
+from processes import advanced_information
 
 # Extras
 import json
 import os
 import sys
 import shutil
+
+#
+#   loadAllData: Carga los datos en la aplicación
+#
+def loadAllData() -> None:
+    configuration.loadConfigurationData()
+    types_rockets.loadRockets()
+    station_requests.loadRequests()
+    launchs.loadLaunchs()
+    advanced_information.loadRecordToday()
 
 #
 #   openMenu: Carga el menú de configuración
@@ -22,7 +37,8 @@ def openMenu() -> None:
         translate.getValue('FILES_MENU.EXIT'),
         translate.getValue('FILES_MENU.LOAD_DATA'),
         translate.getValue('FILES_MENU.SAVE_DATA'),
-        translate.getValue('FILES_MENU.RESET_DATA')
+        translate.getValue('FILES_MENU.RESET_DATA'),
+        translate.getValue('FILES_MENU.DELETE_DATA')
         ]
     common.printMenu(menuOptions)
     selectedOption = common.requestOptionInteger(0, len(menuOptions))
@@ -33,6 +49,8 @@ def openMenu() -> None:
             saveLocalData()
         elif selectedOption == 3:
             resetLocalData()
+        elif selectedOption == 4:
+            deleteLocalData()
         openMenu()
 
 #
@@ -81,22 +99,62 @@ def existsFile(routeFile: str) -> bool:
 def getListDir(routeFile: str) -> [str]:
     return os.listdir(routeFile)
 
+#
+#   loadLocalData: Carga los datos seleccionados en la memoria actual
+#
 def loadLocalData() -> None:
     files: [str] =  getListDir('system_data/save_files')
     if len(files) > 0:
-        for nameFile in files:
-            print('fileName: ' + nameFile)
+        print('\n'+translate.getValue('FILES_MENU.LOAD_DATA_MESSAGE'))
+        common.printMenu(files, False)
+        selectedOption = common.requestOptionInteger(0, len(files))
+        if securityDataMessage():
+            resetData()
+            loadData(files[selectedOption])
     else:
-        print('No hay datos para cargar')
+        print('\n'+translate.getValue('FILES_MENU.DATA_NOT_FOUND'))
 
+#
+#   deleteLocalData: Elimina los datos de la memoria actual
+#
+def deleteLocalData() -> None:
+    files: [str] =  getListDir('system_data/save_files')
+    if len(files) > 0:
+        print('\n'+translate.getValue('FILES_MENU.DELETE_DATA_MESSAGE'))
+        common.printMenu(files, False)
+        selectedOption = common.requestOptionInteger(0, len(files))
+        if securityDataMessage():
+            resetData('system_data/save_files/' + files[selectedOption])
+            print('\n'+translate.getValue('FILES_MENU.DELETE_DATA_COMPLETE'))
+    else:
+        print('\n'+translate.getValue('FILES_MENU.DATA_NOT_FOUND'))
+
+#
+#   saveLocalData: Guarda los datos de la memoria actual en un espacio existente o nuevo
+#
 def saveLocalData()  -> None:
-    print('save')
+    files: [str] =  getListDir('system_data/save_files')
+    print('\n'+translate.getValue('FILES_MENU.SAVE_DATA_MESSAGE'))
+    optionsMenu = [translate.getValue('FILES_MENU.NEW_SLOT')] + files
+    common.printMenu(optionsMenu)
+    selectedOption = common.requestOptionInteger(0, len(optionsMenu))
+    if selectedOption == 0:
+        saveData(requestSaveName())
+    else:
+        resetData('system_data/save_files/' + optionsMenu[selectedOption])
+        saveData(optionsMenu[selectedOption])
 
+#
+#   resetLocalData: Reinicia los datos de la memoria actual
+#
 def resetLocalData() -> None:
     if securityDataMessage():
         resetData()
         loadData('clean_data', 'system_data')
 
+#
+#   resetData: Reinicia los datos
+#
 def resetData(src="system_data/data") -> None:
     if os.path.exists(src):
         files = getListDir(src)
@@ -111,6 +169,9 @@ def resetData(src="system_data/data") -> None:
                     print ('- Error delete file: ' + fileName)
         os.rmdir(src)
 
+#
+#   saveData: Guarda los datos
+#
 def saveData(saveName, src="system_data/data", dst="system_data/save_files") -> None:
     dst = dst + '/' + saveName
     createDir(dst)
@@ -128,6 +189,9 @@ def saveData(saveName, src="system_data/data", dst="system_data/save_files") -> 
             else:
                 print('- File save completed: ' + fileName)
 
+#
+#   loadData: Carga los datos
+#
 def loadData(saveName,  src="system_data/save_files", dst="system_data/data") -> None:
     src = src + '/' + saveName
     createDir(dst)
@@ -144,13 +208,20 @@ def loadData(saveName,  src="system_data/save_files", dst="system_data/data") ->
                 print ('- Error loading file: ' + fileName)
             else:
                 print('- File load completed: ' + fileName)
-    
+    loadAllData()
+
+#
+#   createDir: Crea un directorio
+#
 def createDir(directoryRoute: str) -> None:
     try:
         os.mkdir(directoryRoute)
     except OSError:
         print ('Error creating new directory: ' + directoryRoute)
 
+#
+#   securityDataMessage: Muestra un mensaje de advertencia al usuario
+#
 def securityDataMessage() -> bool:
     print('\n'+translate.getValue('FILES_MENU.ALERT_MESSAGE'))
     menuOptions = [
@@ -160,3 +231,15 @@ def securityDataMessage() -> bool:
     common.printMenu(menuOptions)
     selectedOption = common.requestOptionInteger(0, len(menuOptions))
     return False if selectedOption == 0 else True
+
+#
+#   requestSaveName: Pide al usuario el nombre del guardado
+#   @return (String)
+#
+def requestSaveName() -> str:
+    files: [str] =  getListDir('system_data/save_files')
+    name = common.cleanText(input(translate.getValue('FILES_MENU.SAVE_NAME') + ": "))
+    while (len(name) < 3) or (name in files) or (len(name) > 14):
+        print(translate.getValue('FILES_MENU.SAVE_NAME_ERROR'))
+        name = common.cleanText(input(translate.getValue('FILES_MENU.SAVE_NAME') + ": "))
+    return name
